60f27ab6e25d0177fff7d17b1876a8d1eb2c0a01
vloch <vutheany.loch@ens-lyon.fr>
2015-01-23 15:48:32 +0100

    Import New tools

--- enslyon/FicheIndexation/content/ficheindexation.py
+++ enslyon/FicheIndexation/content/ficheindexation.py
@@ -35,6 +35,8 @@ from Products.ATContentTypes.content import base
 from Products.ATContentTypes.content import schemata
 from Products.ATContentTypes.lib.historyaware import HistoryAwareMixin
 
+from Products.ATExtensions.ateapi import FormattableNamesField
+from Products.ATExtensions.ateapi import FormattableNamesWidget
 
 from Products.Archetypes.utils import contentDispositionHeader
 
